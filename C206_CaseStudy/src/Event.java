410df044e1be0f6a9c7d42d3c4d47ce9a6f9e8a8
Remus Teo <Remus Teo@LAPTOP-TGDLEVCM>
2023-08-10 17:19:03 +0800

    Updated event code

--- C206_CaseStudy/src/Event.java
+++ C206_CaseStudy/src/Event.java
@@ -1,37 +1,35 @@
 public class Event {
-    private String eventID;
-    private String title;
-    private String date;
-    private String location;
-    private String description;
+	private String eventID;
+	private String title;
+	private String date;
+	private String location;
+	private String description;
 
-    public Event(String eventID, String title, String date, String location, String description) {
-        this.eventID = eventID;
-        this.title = title;
-        this.date = date;
-        this.location = location;
-        this.description = description;
-    }
+	public Event(String eventID, String title, String date, String location, String description) {
+		this.eventID = eventID;
+		this.title = title;
+		this.date = date;
+		this.location = location;
+		this.description = description;
+	}
 
-    public String getEventID() {
-        return eventID;
-    }
+	public String getEventID() {
+		return eventID;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getDate() {
-        return date;
-    }
+	public String getDate() {
+		return date;
+	}
 
-    public String getLocation() {
-        return location;
-    }
+	public String getLocation() {
+		return location;
+	}
 
-    public String getDescription() {
-        return description;
-    }
-    
+	public String getDescription() {
+		return description;
+	}
 }
-
